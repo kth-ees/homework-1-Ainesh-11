da40e2649db323ea7a8984ccf6d593f974640a91
Ainesh-11 <ainesh.shailendra.11@gmail.com>
2025-09-15 22:43:49 +0200

    Update multiplier_tb.sv

--- q9/multiplier_tb.sv
+++ q9/multiplier_tb.sv
@@ -1,5 +1,19 @@
-module multiplier_tb;
+`timescale 1ns/1ns
 
-  // complete
+module mult16_tb;
+
+logic [15:0] a, b;
+logic [31:0] y;
+
+mult16 dut(.a(a), .b(b), .y(y));
+
+initial begin
+  for (int i=0; i<10; i++) begin
+    a = $urandom_range(0,65535);
+    b = $urandom_range(0,65535);
+    #10;
+  end
+  $stop;
+end
 
 endmodule
