5e8a4bcaef0a23433209c6af90e4770eb8804e72
Ainesh-11 <ainesh.shailendra.11@gmail.com>
2025-09-15 22:55:10 +0200

    Update csa_8.sv

--- q3/csa_8.sv
+++ q3/csa_8.sv
@@ -1,9 +1,20 @@
-module csa_8 (
-  input logic [7:0] a, b,
-  output logic [7:0] sum,
-  output logic carry
+module CSA_8 (
+  input  logic [7:0] A, B,
+  output logic [7:0] S,
+  output logic       carry
 );
-  // …
-  // Add your description here
-  // …
+
+logic w;
+logic [3:0] s0, s1;
+logic c0, c1;
+
+assign c0 = 0;
+assign c1 = 1;
+
+adder_4 v1(A[3:0], B[3:0], S[3:0], w);
+adder_4 v2(A[7:4], B[7:4], s0, c0);
+adder_4 v3(A[7:4], B[7:4], s1, c1);
+
+assign {carry, S[7:4]} = (w == 0) ? {1'b0, s0} : {1'b1, s1};
+
 endmodule
