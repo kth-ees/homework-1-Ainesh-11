549a7c0f99d25c8190ca9f9e165b65960ebd2243
Ainesh-11 <ainesh.shailendra.11@gmail.com>
2025-09-15 22:55:49 +0200

    Update count_1.sv

--- q4/count_1.sv
+++ q4/count_1.sv
@@ -1,8 +1,12 @@
-module count_1 (
-  input logic [3:0] a,
-  output logic [2:0] out
-);
-  // …
-  // Add your description here
-  // …
+module count_1(input  logic [3:0] a,
+			output logic [2:0] out);
+
+logic [1:0] sum0, sum1;
+logic [2:0] sum2;
+
+assign sum0 = a[0] + a[1];
+assign sum1 = a[2] + a[3];
+assign sum2 = sum0 + sum1;
+assign out = sum2;
+
 endmodule
