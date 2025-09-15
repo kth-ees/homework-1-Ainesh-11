3b10e673e7095c12a480feca3fdf2b328d4ba879
Ainesh-11 <ainesh.shailendra.11@gmail.com>
2025-09-15 22:54:46 +0200

    Update bin2bcd_tb.sv

--- q2/bin2bcd_tb.sv
+++ q2/bin2bcd_tb.sv
@@ -1,9 +1,19 @@
+`timescale 1ns/1ns
+
 module bin2bcd_tb;
 
-  // Testbench signals
-  logic [3:0] binary;
-  logic [3:0] bcd;
-  logic carry;
+logic [3:0] binary_tb;
+logic [3:0] bcd_tb;
+logic carry_tb;
 
-  // complete your tb
+bin2bcd uut (.binary(binary_tb), .bcd(bcd_tb), .carry(carry_tb));
+
+initial begin
+  for (int i = 0; i < 16; i++) begin
+    binary_tb = i;
+    #10;
+  end
+  $stop;
+end
+
 endmodule
