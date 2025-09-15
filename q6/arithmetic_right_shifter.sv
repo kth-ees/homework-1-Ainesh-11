1e3ca9c695a3addbf2f960d830db944d4e2c5e1f
Ainesh-11 <ainesh.shailendra.11@gmail.com>
2025-09-15 22:57:23 +0200

    Update arithmetic_right_shifter.sv

--- q6/arithmetic_right_shifter.sv
+++ q6/arithmetic_right_shifter.sv
@@ -1,8 +1,16 @@
-module arithmetic_right_shifter #(parameter N) (
-  input logic [N-1:0] input_data,
-  input logic [1:0] control,
-  output logic [N-1:0] shifted_result
+module ArithmeticRightShifter #(parameter N = 5) (
+    input  logic [N-1:0] input_data,
+    input  logic [1:0]   control,       
+    output logic [N-1:0] shifted_result
 );
 
-  // complete the module
+    always_comb begin
+        case (control)
+            2'b00: shifted_result = input_data; 
+            2'b01: shifted_result = {input_data[N-1], input_data[N-1:1]};
+            2'b10: shifted_result = {{2{input_data[N-1]}}, input_data[N-1:2]};
+            2'b11: shifted_result = {{3{input_data[N-1]}}, input_data[N-1:3]};
+            default: shifted_result = input_data;
+        endcase
+    end
 endmodule
