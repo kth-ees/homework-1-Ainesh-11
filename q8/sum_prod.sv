a2f841c6c92cb02c63b59972deeaddd000c58e11
Ainesh-11 <ainesh.shailendra.11@gmail.com>
2025-09-15 22:41:14 +0200

    Update sum_prod.sv

--- q8/sum_prod.sv
+++ q8/sum_prod.sv
@@ -1,8 +1,13 @@
-module sum_prod #(parameter N) (
+module sum_prod #(parameter N=4) (
   input  logic [N-1:0] X [5:0],
-  output logic [2N+2:0] result
+  output logic [2*N+2:0] result
 );
 
-// complete the module
+logic [2*N-1:0] p0, p1, p2;
+
+assign p0 = X[0] * X[1];
+assign p1 = X[2] * X[3];
+assign p2 = X[4] * X[5];
+assign result = p0 + p1 + p2;
 
 endmodule
