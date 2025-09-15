872fdda5e71f51ac6369691eaca4c1c25980a921
Ainesh-11 <ainesh.shailendra.11@gmail.com>
2025-09-15 22:42:08 +0200

    Update sum_prod_tb.sv

--- q8/sum_prod_tb.sv
+++ q8/sum_prod_tb.sv
@@ -1,5 +1,19 @@
+`timescale 1ns/1ns
+
 module sum_prod_tb;
 
-  // complete the testbench
+parameter N=4;
+logic [N-1:0] X [5:0];
+logic [2*N+2:0] result;
 
-endmodule;
+sum_prod #(N) dut(.X(X), .result(result));
+
+initial begin
+  for (int i=0; i<10; i++) begin
+    for (int j=0; j<6; j++) X[j] = $urandom_range(0,(1<<N)-1);
+    #10;
+  end
+  $stop;
+end
+
+endmodule
