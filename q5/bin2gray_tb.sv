899706ccb704545da8c32291c1e0e6453c81fc31
Ainesh-11 <ainesh.shailendra.11@gmail.com>
2025-09-15 22:56:53 +0200

    Update bin2gray_tb.sv

--- q5/bin2gray_tb.sv
+++ q5/bin2gray_tb.sv
@@ -1,10 +1,19 @@
-module bin2gray_tb;
+`timescale 1ns/1ns
 
-  // Inputs
-  logic [3:0] binary;
+module bin2bcd_tb;
 
-  // Outputs
-  logic [3:0] gray;
+logic [3:0] binary_tb;
+logic [3:0] bcd_tb;
+logic carry_tb;
 
-  // complete
+bin2bcd uut (.binary(binary_tb), .bcd(bcd_tb), .carry(carry_tb));
+
+initial begin
+  for (int i = 0; i < 16; i++) begin
+    binary_tb = i;
+    #10;
+  end
+  $stop;
+end
+
 endmodule
