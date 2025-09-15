519ab75e0bd7c44d731a2506b3a7cf3fd789cd5b
Ainesh-11 <ainesh.shailendra.11@gmail.com>
2025-09-15 22:54:29 +0200

    Update bin2bcd.sv

--- q2/bin2bcd.sv
+++ q2/bin2bcd.sv
@@ -3,7 +3,27 @@ module bin2bcd (
   output logic [3:0] bcd,
   output logic carry
 );
-  // …
-  // Add your description here
-  // …
+
+always_comb begin
+  case (binary)
+    4'b0000: {bcd,carry} = 5'b00000;
+    4'b0001: {bcd,carry} = 5'b00010;
+    4'b0010: {bcd,carry} = 5'b00100;
+    4'b0011: {bcd,carry} = 5'b00110;
+    4'b0100: {bcd,carry} = 5'b01000;
+    4'b0101: {bcd,carry} = 5'b01010;
+    4'b0110: {bcd,carry} = 5'b01100;
+    4'b0111: {bcd,carry} = 5'b01110;
+    4'b1000: {bcd,carry} = 5'b10000;
+    4'b1001: {bcd,carry} = 5'b10010;
+    4'b1010: {bcd,carry} = 5'b00001;
+    4'b1011: {bcd,carry} = 5'b00011;
+    4'b1100: {bcd,carry} = 5'b00101;
+    4'b1101: {bcd,carry} = 5'b00111;
+    4'b1110: {bcd,carry} = 5'b01001;
+    4'b1111: {bcd,carry} = 5'b01011;
+    default: {bcd,carry} = 5'b00000;
+  endcase
+end
+
 endmodule
