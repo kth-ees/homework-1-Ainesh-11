66bb6f364a94fa80e2f6d1a1e17e8a1111462c89
Ainesh-11 <ainesh.shailendra.11@gmail.com>
2025-09-15 22:56:08 +0200

    Update count_1_tb.sv

--- q4/count_1_tb.sv
+++ q4/count_1_tb.sv
@@ -1,10 +1,17 @@
+`timescale 1ns/1ns
 module count_1_tb;
 
-  // Inputs
-  logic [3:0] a;
+logic [3:0] a_tb;
+logic [2:0] out_tb;
 
-  // Outputs
-  logic [2:0] out;
+count_1 dut(.a(a_tb), .out(out_tb));
 
-  // complete
+initial begin
+  for (int i = 0; i < 16; i++) begin
+    a_tb = i;
+    #10;
+  end
+  $stop;
+end
+
 endmodule
