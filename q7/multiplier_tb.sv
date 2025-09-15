d70b1d6acdb694b86077e3c45093cf838f056f5d
Ainesh-11 <ainesh.shailendra.11@gmail.com>
2025-09-15 22:59:00 +0200

    Update multiplier_tb.sv

--- q7/multiplier_tb.sv
+++ q7/multiplier_tb.sv
@@ -1,6 +1,24 @@
-module multiplier_tb;
+`timescale 1ns/1ps
+module tb_multiplier;
 
-  localparam N = 4;
+localparam N=5;
+logic [N-1:0] a,b;
+logic [2*N-1:0] product;
 
-  // complete
+multiplier #(N) dut(.a(a),.b(b),.product(product));
+initial begin
+	a=5'b00101; b=5'b00011; #5;
+	$display("a=%b (%0d), b=%b (%0d) -> product=%b (%0d)",a,$signed(a),b,$signed(b),product,$signed(product));
+
+	a=5'b11101; b=5'b00011; #5;
+	$display("a=%b (%0d), b=%b (%0d) -> product=%b (%0d)",a,$signed(a),b,$signed(b),product,$signed(product));
+
+	a=5'b11101; b=5'b11110; #5;
+	$display("a=%b (%0d), b=%b (%0d) -> product=%b (%0d)",a,$signed(a),b,$signed(b),product,$signed(product));
+
+	a=5'b01010; b=5'b00101; #5;
+	$display("a=%b (%0d), b=%b (%0d) -> product=%b (%0d)",a,$signed(a),b,$signed(b),product,$signed(product));
+
+	$stop;
+end
 endmodule
